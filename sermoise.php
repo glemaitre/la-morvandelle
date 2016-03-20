c16dd939670f2c8b2cb5692616a4d9fd2c025e52
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2016-03-20 14:34:12 +0100

    Add the document of sud-morvandelle

---
+++ sermoise.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
+
+  <head>
+    <link rel="stylesheet" href="../../css/template.css" type="text/css" />
+    <link href="../../images/icone.ico" rel="shortcut icon" type="image/x-icon" />
+    <meta http-equiv="description" content=".:Site officiel de l'ASC Fours:."><title>.:Site officiel de l'ASC Fours:.</title>
+    </head>
+
+    <body>
+      <div style="overflow: hidden; position: absolute; top: 1px; left: 1px; height: 1px; width: 1px;">
+      </div>
+
+      <div id="header">
+	<div id="headerinside">	
+	  <h1 style="margin:0"><img src="../../images/logo.png" width="410" height="100" style="float:left"/></h1>
+          <h1 style="margin:0"><img src="../../images/logo_ascfours2.png" width="162" height="100" style="float:right"/></h1>
+	  <div id="topinside">		
+	    <div id="topmenu">
+	      
+	    </div>
+	  </div>	
+	</div>
+	
+	<div id="container">	
+
+	  <div id="main">
+	    <div style="padding-right:10px">
+	      <div class="componentheading">
+		Semi-nocturne FFC de Sermoise sur Loire 
+		<span class="article_separator">&nbsp;</span>
+	      </div>
+	      
+	      <br>
+		
+		<table class="blog" cellpadding="0" cellspacing="0">
+		  <tr>
+		    <td valign="top">
+		      <div>
+			
+			<hr color="#cccccc" size="1px">
+			  <center> <b>Samedi 23 Mai 2015</b> </center>
+			  <center> Résumé : circuit de 1,2** km </center>
+			  <center> Dénivelé positif : ** m </center>
+			  <hr color="#cccccc" size="1px">
+			    <center> Pass' Cyclisme D3-D4: Depart 18 h 00 - 1 h 30 + 5 tours</center>
+			    <center> Pass' Cyclisme D1-D2: Depart 18 h 00 - 1 h 30 + 10 tours</center>
+			    <hr color="#cccccc" size="1px">
+
+			      <table class="contentpaneopen">
+
+				<tr>
+				  
+				  <!-- <center><script type="text/javascript" src="http://www.openrunner.com/orservice/inorser-script.php?key=mykey&amp;ser=S09&amp;id=2354393&amp;w=415&amp;h=350&amp;hp=128&amp;k=5&amp;m=0&amp;pa=0&amp;c=0&amp;ts=1427057995"></script></center>  -->
+				  
+				</tr>
+
+			      </table>
+			      
+			      <span class="article_separator">&nbsp;</span>
+			    </div>
+			  </tr>
+
+
+			</table>
+			<div align="center" class="breadcrumbs">
+			</div>
+		      </div>
+		    </div>
+		    
+		    <div id="leftsidebar">
+		      <div class="moduletable_menu">
+			<h3>Menu principal</h3>
+			<ul class="menu"><li id="current" class="active item1"><a href="../../index.php">Home</a></li></ul>
+			<ul class="menu"><li id="current" class="active item1"><a href="../club.php"><span>Club</span></a></li></ul>
+			<div class="submenu">
+			  <ul class="menu"><li id="current" class="active item1"><a href="organisations.php"><span>Organisations</span></a></li></ul>
+			  <ul class="menu"><li id="current" class="active item1"><a href="Montambert.php"><span>Montambert</span></a></li></ul>	
+			  <ul class="menu"><li id="current" class="active item1"><a href="chatillon.php"><span>Chatillon en Bazois</span></a></li></ul>
+			  <ul class="menu"><li id="current" class="active item1"><a href="sermoise.php"><span>Sermoise sur Loire</span></a></li></ul>
+			  <ul class="menu"><li id="current" class="active item1"><a href="luzy.php"><span>Luzy</span></a></li></ul>
+			  <!--<ul class="menu"><li id="current" class="active item1"><a href="montsauche.php"><span>Montsauche Les Settons</span></a></li></ul>-->
+			  <!--<ul class="menu"><li id="current" class="active item1"><a href="fours1.php"><span>Fours 12 mai</span></a></li></ul>-->
+			  <!--<ul class="menu"><li id="current" class="active item1"><a href="foursart.php"><span>Fours 30 juin</span></a></li></ul>-->
+			  <ul class="menu"><li id="current" class="active item1"><a href="chiddes.php"><span>Chiddes</span></a></li></ul>
+			  <ul class="menu"><li id="current" class="active item1"><a href="vetathlon.php"><span>Vetathlon Fours</span></a></li></ul>
+			</div>
+			<ul class="menu"><li id="current" class="active item1"><a href="../sponsoring.php"><span>Sponsoring</span></a></li></ul>
+			<ul class="menu"><li id="current" class="active item1"><a href="../contact.php"><span>Contact</span></a></li></ul>
+		      </div>
+		    </div>	
+
+
+		    <div style="clear:both"></div>
+		  </div>
+
+		  <div id="footer">
+		    Webmaster <a href="http://g.lemaitre58.free.fr">Guillaume Lemaître</a> - <a href="../login.php">Espace privee</a>
+		  </div>
+		</body>
+	      </html>
